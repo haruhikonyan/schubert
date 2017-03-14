a01d6062e0e0cea02964556e6048af4edb38fcf8
haruhiko <haruhiko@haruhiko-no-MacBook-Air.local>
2017-03-14 11:29:09 +0900

    Type の CRAD 適当ver

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  resources :types
 end
