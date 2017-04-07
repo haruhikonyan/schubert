51b8feffda1167e0b06c1cd3311f86890259fabe
haruhiko <haruhiko@haruhiko-no-MacBook-Air.local>
2017-04-07 11:32:56 +0900

    団体関連のページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   resources :types
+  resources :teams
   resources :recruits
   post 'recruits/:id/edit', to: 'recruits#edit'
 
