49ba4a3ddcf2b0fe1774e0b0bc290321a0dd13a4
haruhiko <haruhiko@haruhiko-no-MacBook-Air.local>
2017-04-11 00:35:48 +0900

    devise で team 追加 passwordカラム削除

--- app/models/team.rb
+++ app/models/team.rb
@@ -13,6 +13,10 @@
 #
 
 class Team < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   # SecurePassword有効
   has_secure_password
 
--- db/migrate/20170311085447_create_teams.rb
+++ db/migrate/20170311085447_create_teams.rb
@@ -2,7 +2,6 @@ class CreateTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :teams do |t|
       t.string :name, null: false
-      t.string :password_digest, null: false
       t.string :mail
       t.string :url
       t.text :free_text
