61a5b99b1bc90e4c78427d2659b3f2fcdf79217c
haruhiko <haruhiko@haruhiko-no-MacBook-Air.local>
2017-04-20 13:09:45 +0900

    すばげってぃもんすたー 団体管理ページから募集を作れるようにする

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -1,5 +1,6 @@
 class RecruitsController < ApplicationController
   before_action :set_recruit, only: [:show, :edit, :update, :destroy]
+  before_action :set_team, only: [:new, :edit]
   before_action :reset_password_session, only: [:show, :index]
 
   # GET /recruits
@@ -20,12 +21,11 @@ class RecruitsController < ApplicationController
 
   # GET /recruits/new
   def new
-    @recruit = Recruit.new(team: Team.new, published_from: DateTime.now, published_to: DateTime.now)
+    @recruit = Recruit.new(published_from: DateTime.now, published_to: DateTime.now)
   end
 
   # POST /recruits/1/edit
   def edit
-    @team = @recruit.team
     session[:password] = params[:password]
     redirect_to @recruit, alert: 'パスワードが違います' unless @recruit.team.authenticate(session[:password])
   end
@@ -33,9 +33,9 @@ class RecruitsController < ApplicationController
   # POST /recruits
   # POST /recruits.json
   def create
-    team = Team.new(team_params)
+    @team = Team.new(team_params) if @team
     @recruit = Recruit.new(recruit_params)
-    @recruit.team = team
+    @recruit.team = @team
 
     respond_to do |format|
       if @recruit.save
@@ -77,6 +77,10 @@ class RecruitsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_recruit
       @recruit = Recruit.find(params[:id])
+    end
+
+    def set_team
+      params[:team_id] ? @team = Team.find(params[:team_id]) : @team = @recruit&.team
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
