845c9fea32346daea443a8e0b7bb39a6f7a6b421
marinkyo <haruhiko@weseek.co.jp>
2017-05-27 20:48:23 +0900

    かなりお手上げ状態 viewはいい感じなんだけどなぁ

--- app/models/questionnaire_item_answer.rb
+++ app/models/questionnaire_item_answer.rb
@@ -15,4 +15,5 @@ class QuestionnaireItemAnswer < ApplicationRecord
   belongs_to :questionnaire_answer
   has_many :questionnaire_choices_options
   has_many :answer_choices_options, :through => :questionnaire_choices_options
+
 end
